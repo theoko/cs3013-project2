b32de25479f91ed511f692d8e33ead700447cf32
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-02-03 14:58:52 -0500

    oso blepoun patriots emeis douleuoume

--- tests/testcalls.c
+++ tests/testcalls.c
@@ -3,21 +3,36 @@
 #include <unistd.h>
 // These values MUST match the unistd_32.h modifications:
 #define __NR_cs3013_syscall1 377
-#define __NR_cs3013_syscall2 378
-#define __NR_cs3013_syscall3 379
+
 long testCall1 ( void) {
 return (long) syscall(__NR_cs3013_syscall1);
 }
-long testCall2 ( void) {
-return (long) syscall(__NR_cs3013_syscall2);
-}
-long testCall3 ( void) {
-return (long) syscall(__NR_cs3013_syscall3);
-}
+
+
 int main () {
 printf("The return values of the system calls are:\n");
 printf("\tcs3013_syscall1: %ld\n", testCall1());
-printf("\tcs3013_syscall2: %ld\n", testCall2());
-printf("\tcs3013_syscall3: %ld\n", testCall3());
+
+if(fopen("virus", "r") == NULL || fopen("not_virus", "r") == NULL) {
+	printf("FILES DO NOT EXIST");
+	return -1;
+}
+
+char buffer1[200];
+char buffer2[200];
+
+FILE* virus;
+FILE* not_virus;
+
+virus = fopen("virus", "r");
+fscanf(virus, "%s", buffer1);
+
+fclose(virus);
+
+not_virus = fopen("not_virus", "r");
+fscanf(not_virus, "%s", buffer2);
+
+fclose(not_virus);
+
 return 0;
 }
